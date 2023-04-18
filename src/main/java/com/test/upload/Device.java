ba37a33cfc06d48abbedd8fea3df88093002cab3
Lesible <1419318498@qq.com>
2023-04-18 15:46:54 +0800

    init

---
+++ src/main/java/com/test/upload/Device.java
@@ -0,0 +1,18 @@
+package com.test.upload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Device {
+
+    private String deviceCode;
+
+    private String deviceName;
+
+}
--- src/main/java/com/test/upload/DragFrame.java
+++ src/main/java/com/test/upload/DragFrame.java
@@ -11,22 +11,34 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetAdapter;
 import java.awt.dnd.DropTargetDropEvent;
+import java.awt.event.*;
 import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DragFrame extends JFrame {
     private static final Logger log = LoggerFactory.getLogger(DragFrame.class);
+    private static final String url = "http://101.37.75.26:8080/hac_auto/uploadFile?param_name=upLoadFile";
+    private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     JPanel jp1;
     TextArea textArea;
+    TextField valueField;
 
-    JComboBox<Station> jComboBox;
+    TextField textField;
+    JComboBox<Station> jComboBox1;
+    JComboBox<Device> jComboBox2;
 
-    private static final String url = "http://101.37.75.26:8080/hac_auto/uploadFile?param_name=upLoadFile";
+    JPopupMenu pop;
+
+    JMenuItem paste = new JMenuItem("粘贴");
 
     public DragFrame() {
 
@@ -39,9 +51,13 @@ public class DragFrame extends JFrame {
         setTitle("拖拽文件进入进行上传");
         Station hm = new Station("3301010001", "浙江环茂测试站点");
         Station sh = new Station("3301010502", "无人运维站点-申昊");
-        jComboBox = new JComboBox<>(new DefaultComboBoxModel<>(new Station[]{hm, sh}));
-        jComboBox.setSelectedItem(sh);
-        jComboBox.setRenderer(new BasicComboBoxRenderer() {
+        Device cod = new Device("002", "COD分析仪");
+        Device an = new Device("006", "氨氮分析仪");
+        Device p = new Device("010", "总磷分析仪");
+        Device n = new Device("014", "总氮分析仪");
+        jComboBox1 = new JComboBox<>(new DefaultComboBoxModel<>(new Station[]{hm, sh}));
+        jComboBox1.setSelectedItem(sh);
+        jComboBox1.setRenderer(new BasicComboBoxRenderer() {
             @Override
             public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
                 Station station = (Station) value;
@@ -49,19 +65,76 @@ public class DragFrame extends JFrame {
                 return this;
             }
         });
+        jComboBox2 = new JComboBox<>(new DefaultComboBoxModel<>(new Device[]{cod, an, p, n}));
+        jComboBox2.setSelectedItem(cod);
+        jComboBox2.setRenderer(new BasicComboBoxRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                Device device = (Device) value;
+                setText(device.getDeviceName());
+                return this;
+            }
+        });
+        textField = new TextField(20);
+        valueField = new TextField(6);
+        jp1.add(valueField);
+        jp1.add(textField);
+        MouseListener mouseListener = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    paste.setEnabled(true);
+                    pop.show(textField, e.getX(), e.getY());
+                }
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        };
+        textField.addMouseListener(mouseListener);
+        pop = new JPopupMenu();
+        pop.add(paste);
+        paste.setAccelerator(KeyStroke.getKeyStroke('V', InputEvent.CTRL_MASK));
+        paste.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    textField.setText(Toolkit.getDefaultToolkit()
+                            .getSystemClipboard().getContents(null)
+                            .getTransferData(DataFlavor.stringFlavor).toString());
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
         textArea = new TextArea(30, 144);
         textArea.setBounds(20, 20, 500, 600);
-        jp1.add(jComboBox);
+        jp1.add(jComboBox1);
+        jp1.add(jComboBox2);
+//        Container parent = textArea.getParent();
+//        parent.setComponentZOrder(parent,0);
         jp1.add(textArea, BorderLayout.CENTER);
         drag();
     }
 
     public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (Exception e) {
-            log.error("error", e);
-        }
         new DragFrame().setVisible(true);
 
     }
@@ -72,7 +145,6 @@ public class DragFrame extends JFrame {
             @Override
             public void drop(DropTargetDropEvent dtde) {
                 try {
-
                     if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
                         dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
                         List<File> list = (List<File>) (dtde.getTransferable().getTransferData(DataFlavor.javaFileListFlavor));
@@ -114,8 +186,13 @@ public class DragFrame extends JFrame {
     }
 
     private String buildSql(List<UploadResult> results) {
-        Station station = (Station) jComboBox.getSelectedItem();
+        Station station = (Station) jComboBox1.getSelectedItem();
+        String text = textField.getText();
+        String value = valueField.getText();
+        LocalDateTime time = LocalDateTime.parse(text, DTF);
         String stationCode = station.getStationCode();
+        Device device = (Device) jComboBox2.getSelectedItem();
+        String deviceCode = device.getDeviceCode();
         StringBuilder sb = new StringBuilder("insert into t_sys_file (old_file_name,new_file_name,login_id,file_att) values");
         int size = results.size();
         for (int i = 0; i < size; i++) {
@@ -136,6 +213,10 @@ public class DragFrame extends JFrame {
                 sb.append(",");
             }
         }
+        time = time.plusMinutes(59L);
+        sb.append("insert into b_task_result (task_code, get_time, result_value, result_desc, station_code, device_code, s_desc)\n")
+                .append(String.format("values ('A060601', '%s', 1, '%s', '%s', '%s', '')",
+                        DTF.format(time), value, stationCode, deviceCode));
         return sb.toString();
     }
 
